73e37b36f6a11719174e4fcfef05507810bd7bac
Morgan-LC <36822990+Morgan-LC@users.noreply.github.com>
2026-04-24 10:34:41 +1100

    update deploy conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/fruits-legumes-nc/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
